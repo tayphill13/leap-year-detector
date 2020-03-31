f39e714c7790b29c94d3cf673d594483bb50cd73
Taylor Phillips <taylorphillips133@gmail.com>
2020-03-31 15:28:45 -0700

    add divisible bt 400

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 
 // business logic
 var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
