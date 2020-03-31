049dc5cec9a8c0d4c4acdb0b35b5b8cddccf504d
Taylor Phillips <taylorphillips133@gmail.com>
2020-03-31 14:56:17 -0700

    add our first function and var leapYear

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// user interface logic
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+
+    var leapYear = function(year) {
+      return false;
+    }
+  });
+});
